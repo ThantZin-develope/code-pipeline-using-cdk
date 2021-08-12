b71b8966c0456a665f349723140e11f3213f6f15
ThantZin Tun <thantzin-tun@systemexe-myanmar.com>
2021-08-12 10:26:30 +0630

    update app functionalities

--- lib/aws-tests-stack.ts
+++ lib/aws-tests-stack.ts
@@ -1,9 +1,28 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as apigw from '@aws-cdk/aws-apigateway';
+import * as path from 'path';
+
 
 export class AwsTestsStack extends cdk.Stack {
+
+  public readonly hcEndpoint: cdk.CfnOutput;
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset(path.resolve(__dirname, '../lambda')),
+      handler: 'hello.handler',
+    });
+
+    const gateway = new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello
+    });
+
+    this.hcEndpoint = new cdk.CfnOutput(this, 'GatewayUrl', {
+      value: gateway.url
+    });
+
   }
 }
